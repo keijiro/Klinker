52972b15e498182d4544829b0f6b6be52ca56ae9
Keijiro Takahashi <keijiro@gmail.com>
2018-12-03 11:46:47 +0900

    Add feedback test

---
+++ Assets/Test/Misc/Flasher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Flasher : MonoBehaviour
+{
+    [SerializeField] Color _color1 = Color.black;
+    [SerializeField] Color _color2 = Color.white;
+    [SerializeField, Range(1, 10)] int _interval = 1;
+
+    IEnumerator Start()
+    {
+        var cam = GetComponent<Camera>();
+
+        while (true)
+        {
+            cam.backgroundColor = _color1;
+            for (var i = 0; i < _interval; i++) yield return null;
+
+            cam.backgroundColor = _color2;
+            for (var i = 0; i < _interval; i++) yield return null;
+        }
+    }
+}
